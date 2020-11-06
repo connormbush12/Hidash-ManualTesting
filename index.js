d183da4f72ae4e8a49ae3cf58c79c2c237ebb76f
Connor Bush <connormbush12@gmail.com>
2020-11-06 11:04:23 -0600

    Created map array callback method clone with test

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-//This is a very simple recreation of the array method forEach.
 module.exports = {
     forEach(arr, fn) {
         for (let i=0; i<arr.length; i++) {
             const value = arr[i];
             fn(value, i);
         }
+    },
+
+    //Now, we recreated the map function. It esentially does the same thing as forEach, but it pushes the result into a new array.
+    map(arr, fn) {
+        //We have to establish result outside of the for loop so that we can return it outside of the for loop
+        const result = [];
+        for (let i=0; i<arr.length; i++) {
+            result.push(fn(arr[i], i));
+        }
+        return result;
     }
 }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 //This is our custom testing of forEach.
-const {forEach} = require('./index')
+const {forEach, map} = require('./index')
 
 let sum = 0;
 forEach([1, 2, 3], (value) => {
@@ -9,4 +9,22 @@ forEach([1, 2, 3], (value) => {
 //With our test case above, we expect the sum to be 6. Therefore, we throw an error if the sum is NOT 6. If it is, we don't do anything, which indicates the test was successful.
 if (sum !== 6) {
     throw new Error('Expected sum of array was 6')
+}
+
+//This is our custom testing of map.
+const result = map([1, 2, 3], value => {
+    return value*2;
+})
+
+//With our test case above, we expect the result array to bo [2,4,6]. Therefore, we throw an error if either of those array elements is incorrect. If it is, we don't do anything, which indicates the test was successful.
+if(result[0] !== 2) {
+    throw new Error(`Expected result to be 2 but received ${result[0]}`)
+}
+
+if(result[1] !== 4) {
+    throw new Error(`Expected result to be 4 but received ${result[1]}`)
+}
+
+if(result[2] !== 6) {
+    throw new Error(`Expected result to be 6 but received ${result[2]}`)
 }
