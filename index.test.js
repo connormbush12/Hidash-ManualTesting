c74fd2a4331008f7cd953fd93b7e934c5842af92
Connor Bush <connormbush12@gmail.com>
2020-11-06 15:26:36 -0600

    Use Mocha JS

--- index.test.js
+++ index.test.js
@@ -2,30 +2,29 @@
 const assert = require('assert')
 const {forEach, map} = require('./index')
 
+//Instead of using our custom test function (commented out below), we can use Mocha JS. We install it on our machines. Then to test our code, in the terminal instead of using "node index.test.js", we use "mocha index.test.js"
 
-const test = (desc, fn) => {
-    console.log('----', desc);
-    try {
-        fn();
-    } catch (err) {
-        console.log(err.message)
-    }
-}
+// const test = (desc, fn) => {
+//     console.log('----', desc);
+//     try {
+//         fn();
+//     } catch (err) {
+//         console.log(err.message)
+//     }
+// }
 
-test('The forEach function', () => {
+//The only thing we have to do to use Mocha is change our test function to 'it'
+it('The forEach function', () => {
     let sum = 0;
     forEach([1, 2, 3], (value) => {
         sum += value;
     })
-    //Using assert.strictEqual, we are saying "throw an error if sum is not strictly equal to 6"
     assert.strictEqual(sum, 6)
 })
 
-test('The map function', () => {
+it('The map function', () => {
     const result = map([1, 2, 3], value => {
         return value*2;
     })
-    
-    //With assert.deepStrictEqual, we are saying "throw an error if result is not an array with the elements: 2, 4, and 6 (in that order)"
     assert.deepStrictEqual(result, [2,4,6])
 })
