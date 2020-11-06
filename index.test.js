b856eb3672b9f123979b8f46d50056aa5fda1d4c
Connor Bush <connormbush12@gmail.com>
2020-11-06 14:40:48 -0600

    Use Assertion Testing library to simplify code

--- index.test.js
+++ index.test.js
@@ -1,16 +1,10 @@
+//The Node JS library Assertion Testing allows us to simplify our validation checking for our testing. As opposed to using if statements over and over, we can use the Assertion Testing methods assert.strictEqual and assert.deepStrictEqual
+const assert = require('assert')
 const {forEach, map} = require('./index')
 
-//There are three current issues with our testing file:
-//1. Can't reuse variable names
-//2. If we throw an error, it doesn't execute the rest of the tests in our test file
-//3. No indication of what test is being run
 
-//We can solve all three of these by utilizing a testing function
-//We pass through two arguments, a description of the test we are about to run and a function
 const test = (desc, fn) => {
-    //We console.log the description. This solves issue #3 listed above
     console.log('----', desc);
-    //Instead of simply running the function, we use a try and catch block. That way, if we do hit an error, we catch it and simply console.log it as opposed to ending the execution of the file. This solves issue #2
     try {
         fn();
     } catch (err) {
@@ -19,14 +13,12 @@ const test = (desc, fn) => {
 }
 
 test('The forEach function', () => {
-    //Finally, since these are scoped within a function now, we can reuse variables such as 'sum' or 'result' over and over again
     let sum = 0;
     forEach([1, 2, 3], (value) => {
         sum += value;
     })
-    if (sum !== 6) {
-        throw new Error('Expected sum of array was 6')
-    }
+    //Using assert.strictEqual, we are saying "throw an error if sum is not strictly equal to 6"
+    assert.strictEqual(sum, 6)
 })
 
 test('The map function', () => {
@@ -34,15 +26,6 @@ test('The map function', () => {
         return value*2;
     })
     
-    if(result[0] !== 2) {
-        throw new Error(`Expected result to be 2 but received ${result[0]}`)
-    }
-    
-    if(result[1] !== 4) {
-        throw new Error(`Expected result to be 4 but received ${result[1]}`)
-    }
-    
-    if(result[2] !== 6) {
-        throw new Error(`Expected result to be 6 but received ${result[2]}`)
-    }
+    //With assert.deepStrictEqual, we are saying "throw an error if result is not an array with the elements: 2, 4, and 6 (in that order)"
+    assert.deepStrictEqual(result, [2,4,6])
 })
